15ab73a1184795567acf941f37c17891c3f6d000
caguss <dipsy1014@naver.com>
2023-04-13 14:40:11 +0900

    초기 커밋.

---
+++ MQTTServer/LogCenter.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MQTTServer
+{
+    partial class LogCenter
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlp_LOG = new System.Windows.Forms.TableLayoutPanel();
+            this.lb_LOG = new System.Windows.Forms.ListBox();
+            this.btn_LOGClear = new System.Windows.Forms.Button();
+            this.btn_LOGExport = new System.Windows.Forms.Button();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.tlp_LOG.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlp_LOG
+            // 
+            this.tlp_LOG.ColumnCount = 2;
+            this.tlp_LOG.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlp_LOG.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlp_LOG.Controls.Add(this.lb_LOG, 0, 0);
+            this.tlp_LOG.Controls.Add(this.btn_LOGClear, 0, 1);
+            this.tlp_LOG.Controls.Add(this.btn_LOGExport, 1, 1);
+            this.tlp_LOG.Controls.Add(this.btn_Open, 0, 2);
+            this.tlp_LOG.Controls.Add(this.btn_Close, 1, 2);
+            this.tlp_LOG.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlp_LOG.Location = new System.Drawing.Point(0, 0);
+            this.tlp_LOG.Name = "tlp_LOG";
+            this.tlp_LOG.RowCount = 3;
+            this.tlp_LOG.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlp_LOG.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tlp_LOG.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tlp_LOG.Size = new System.Drawing.Size(584, 611);
+            this.tlp_LOG.TabIndex = 2;
+            // 
+            // lb_LOG
+            // 
+            this.tlp_LOG.SetColumnSpan(this.lb_LOG, 2);
+            this.lb_LOG.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lb_LOG.FormattingEnabled = true;
+            this.lb_LOG.ItemHeight = 12;
+            this.lb_LOG.Location = new System.Drawing.Point(3, 3);
+            this.lb_LOG.Name = "lb_LOG";
+            this.lb_LOG.Size = new System.Drawing.Size(578, 505);
+            this.lb_LOG.TabIndex = 0;
+            // 
+            // btn_LOGClear
+            // 
+            this.btn_LOGClear.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_LOGClear.Location = new System.Drawing.Point(3, 514);
+            this.btn_LOGClear.Name = "btn_LOGClear";
+            this.btn_LOGClear.Size = new System.Drawing.Size(286, 44);
+            this.btn_LOGClear.TabIndex = 1;
+            this.btn_LOGClear.Text = "LOG Clear";
+            this.btn_LOGClear.UseVisualStyleBackColor = true;
+            this.btn_LOGClear.Click += new System.EventHandler(this.btn_LOGClear_Click);
+            // 
+            // btn_LOGExport
+            // 
+            this.btn_LOGExport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_LOGExport.Location = new System.Drawing.Point(295, 514);
+            this.btn_LOGExport.Name = "btn_LOGExport";
+            this.btn_LOGExport.Size = new System.Drawing.Size(286, 44);
+            this.btn_LOGExport.TabIndex = 2;
+            this.btn_LOGExport.Text = "LOG Export";
+            this.btn_LOGExport.UseVisualStyleBackColor = true;
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_Open.Location = new System.Drawing.Point(3, 564);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(286, 44);
+            this.btn_Open.TabIndex = 3;
+            this.btn_Open.Text = "Open";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_Close.Location = new System.Drawing.Point(295, 564);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(286, 44);
+            this.btn_Close.TabIndex = 4;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // LogCenter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 611);
+            this.Controls.Add(this.tlp_LOG);
+            this.Name = "LogCenter";
+            this.Text = "LOG Center - Server";
+            this.tlp_LOG.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlp_LOG;
+        public System.Windows.Forms.ListBox lb_LOG;
+        private System.Windows.Forms.Button btn_LOGClear;
+        private System.Windows.Forms.Button btn_LOGExport;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Button btn_Close;
+    }
+}
+
---
+++ MQTTServer/LogCenter.cs
@@ -0,0 +1,131 @@
+﻿using MQTTnet.Server;
+using MQTTnet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MQTTnet.Diagnostics;
+using System.Reflection.Emit;
+
+namespace MQTTServer
+{
+    public partial class LogCenter : Form
+    {
+        MqttServer server;
+        public static ListBox LOGLIST;
+
+        new Dictionary<string, string> users = new Dictionary<string,string>(); 
+
+        public LogCenter()
+        {
+            InitializeComponent();
+            LOGLIST = lb_LOG;
+            //users.Add("asha", "1234"); // username, password
+            //users.Add("sepehr", "5678");
+        }
+
+        private async void btn_Open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btn_Close.Enabled = true;
+                btn_Open.Enabled = false;
+                LOGLIST.Items.Insert(0, "Broker Starting...");
+                await OpenServer();
+            }
+            catch (Exception ex)
+            {
+                btn_Close.Enabled = false;
+                btn_Open.Enabled = true;
+                LOGLIST.Items.Add(ex.Message);
+
+            }
+        }
+
+        private async Task OpenServer()
+        {
+            // Create the options for MQTT Broker
+            var options = new MqttServerOptionsBuilder()
+                //Set endpoint to localhost
+                .WithDefaultEndpoint();
+              //.WithDefaultEndpointPort(5004);
+            // Create a new mqtt server
+            server = new MqttFactory().CreateMqttServer(options.Build());
+            //Add Interceptor for logging incoming messages
+            server.InterceptingPublishAsync += Server_InterceptingPublishAsync;
+            // Add connection Validator
+            //server.ValidatingConnectionAsync += Server_ValidatingConnectionAsync;
+            // Start the server
+            await server.StartAsync();
+            if (server.IsStarted)
+                LOGLIST.Items.Insert(0, "Broker Started");
+            else 
+                LOGLIST.Items.Insert(0, "Broker Start Failed");
+
+            // Keep application running until user press a key
+          
+
+            Task Server_InterceptingPublishAsync(InterceptingPublishEventArgs arg)
+            {
+                // Convert Payload to string
+                var payload = arg.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(arg.ApplicationMessage?.Payload);
+                LOGLIST.Invoke(new MethodInvoker(delegate ()
+                {
+                    LOGLIST.Items.Insert(0,
+                   string.Format("TimeStamp: {0} -- Message: ClientId = {1}, Topic = {2}, Payload = {3}, QoS = {4}, Retain-Flag = {5}",
+                    DateTime.Now,
+                    arg.ClientId,
+                    arg.ApplicationMessage?.Topic,
+                    payload,
+                    arg.ApplicationMessage?.QualityOfServiceLevel,
+                    arg.ApplicationMessage?.Retain));
+                }));
+
+                return Task.CompletedTask;
+            }
+
+            //Task Server_ValidatingConnectionAsync(ValidatingConnectionEventArgs arg)
+            //{
+            //    if (!string.IsNullOrWhiteSpace(arg.Username) && !string.IsNullOrWhiteSpace(arg.Password))
+            //    {
+            //        if (!(users.TryGetValue(arg.Username, out var password) && password == arg.Password))
+            //        {
+            //            arg.ReasonCode = MQTTnet.Protocol.MqttConnectReasonCode.BadUserNameOrPassword;
+            //            return Task.CompletedTask;
+            //        }
+            //    }
+            //    arg.ReasonCode = MQTTnet.Protocol.MqttConnectReasonCode.BadAuthenticationMethod;
+            //    return Task.CompletedTask;
+            //}
+        }
+
+        private async void btn_Close_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btn_Close.Enabled = false;
+                btn_Open.Enabled = true;
+                LOGLIST.Items.Insert(0, "Broker Close.");
+                await server.StopAsync();
+            }
+            catch (Exception ex)
+            {
+                btn_Close.Enabled = true;
+                btn_Open.Enabled = false;
+                LOGLIST.Items.Insert(0, ex.Message);
+
+            }
+        }
+
+        private void btn_LOGClear_Click(object sender, EventArgs e)
+        {
+            LOGLIST.Items.Clear();
+        }
+    }
+
+}
